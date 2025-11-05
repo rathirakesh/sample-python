fd3f26d1abc35f074999841ae6de9ad8d5f9f895
Rakesh Rathi <rathirakesh7@gmail.com>
2025-11-05 13:31:18 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+"""
+Utility functions for the sample Python project.
+"""
+
+
+def add_two_numbers(a, b):
+    """
+    Add two numbers.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Sum of a and b
+    """
+    return a + b
+
